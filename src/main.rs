abbd6540188ff041e60a28a14b61c3200653c814
Taylor Hogge <hoggetaylor@gmail.com>
2018-12-07 16:42:55 -0700

    Read brainfuck source from file instead of stdin

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,16 @@ use crate::parse::parse;
 use crate::compile::compile;
 use crate::interp::interpret;
 
-use std::io::{self, Read};
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).expect("Failed to read input");
-    let tokenized = tokenize(&input);
+    let file_name = env::args().nth(1).expect("Please provide a source file.");
+    let mut source = String::new();
+    let mut f = File::open(file_name).expect("Could not open file");
+    f.read_to_string(&mut source).expect("Failed to read input file");
+    let tokenized = tokenize(&source);
     let parsed = parse(&tokenized).expect("Failed to parse input");
     let compiled = compile(&parsed, 0);
     interpret(compiled);
